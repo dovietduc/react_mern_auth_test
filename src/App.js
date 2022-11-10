1ce4816be8d941399190b6ec45784ddeeec1414a
duc do <tvietduc122@gmail.com>
2022-11-10 19:03:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Register from './components/Register';
+import Index from './components/Index';
+import Login from './components/Login';
+import Admin from './components/Admin';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+			<Routes>
+				<Route path="/" element={<Index />}/>
+				<Route path="/register" element={<Register />}/>
+				<Route path="/login" element={<Login />}/>
+				<Route path="/admin" element={<Admin />}/>
+			</Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/layout/AdminLayout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AdminLayout() {
+    return (
+        "123"
+    );
+}
+
+export default AdminLayout;
